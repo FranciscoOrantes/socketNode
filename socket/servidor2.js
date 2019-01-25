9a54507293cd210ad37673825602c57645ddcb4f
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2019-01-25 10:02:42 -0600

    joj

--- socket/servidor2.js
+++ socket/servidor2.js
@@ -7,7 +7,7 @@ const socket = require('socket.io')(server)
 const {StringDecoder} =  require('string_decoder')
 const decoder = new StringDecoder('utf8')
 
-var HOST = "http://servidor2up.herokuapp.com/"
+var HOST = "https://servidor2up.herokuapp.com/"
 var PORT = 46025
 
   server.listen(PORT, function(){
