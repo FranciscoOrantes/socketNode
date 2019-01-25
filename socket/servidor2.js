46336a5633cd377fa1e8e6f3af7e2492cfd5262c
FranciscoOrantes <173222@ids.upchiapas.edu.mx>
2019-01-25 10:29:21 -0600

    lo que explico el profe

--- socket/servidor2.js
+++ socket/servidor2.js
@@ -1,5 +1,18 @@
 var express = require('express')
 var aplicacion = express()
+//Se le agrego el 25 01 19
+const os = require('os')
+var interface = os.networkInterfaces();
+var ipdinamica;
+ for(var k in interface){
+for(var k2 in interface[k]){
+  var address= interface[k][k2]
+ if(address.family=='IPv4' && !address.internal){
+ ipdinamica= address.address.toString();
+ console.log("La ip que me genero Heroku : " + ipdinamica)
+ }
+}
+}
 
 const net = require('net')
 const server = require('http').Server(aplicacion)
@@ -7,7 +20,7 @@ const socket = require('socket.io')(server)
 const {StringDecoder} =  require('string_decoder')
 const decoder = new StringDecoder('utf8')
 
-var HOST = "https://servidor2up.herokuapp.com/"
+var HOST = ipdinamica
 var PORT = process.env.PORT || 5000;
 
   server.listen(PORT, function(){
